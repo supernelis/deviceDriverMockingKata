e81b25650254891deb51edafa8a6d4de5e660e23
Görge Albrecht <goerge@code-mentor.com>
2020-02-09 14:24:58 +0100

    Add Java setup w/ dummy failing test

---
+++ Java/src/test/java/devicedriver/DeviceDriverTest.java
@@ -0,0 +1,14 @@
+package devicedriver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class DeviceDriverTest {
+
+  @Test
+  void foo() {
+    assertThat(true, is(false));
+  }
+}
