e117f160cb0b987b88f81081ca7d785873a39d9a
Nelis Boucké <nelis@archiwise.com>
2021-10-05 20:33:52 +0200

    Cleanup Sinon part

--- JavaScript/test/devicedriver_sinon_test.js
+++ JavaScript/test/devicedriver_sinon_test.js
@@ -37,23 +37,6 @@ describe("Device Driver", function () {
         driver = new DeviceDriver(hardware);
     });
 
-
-    // "test should call all subscribers when exceptions": function () {
-    //     var myAPI = { method: function () {} };
-    //
-    //     var spy = sinon.spy();
-    //     var mock = sinon.mock(myAPI);
-    //     mock.expects("method").once().throws();
-    //
-    //     PubSub.subscribe("message", myAPI.method);
-    //     PubSub.subscribe("message", spy);
-    //     PubSub.publishSync("message", undefined);
-    //
-    //     mock.verify();
-    //     assert(spy.calledOnce);
-    // }
-
-
     it("reads an address", function () {
         const hardware = sinon.createStubInstance(FlashMemoryDevice);
         hardware.read.returns(A_VALUE);
@@ -81,72 +64,6 @@ describe("Device Driver", function () {
         [address, data] = hardware.write.secondCall.args;
         expect(address).to.equal(AN_ADDRESS);
         expect(data).to.equal(A_VALUE);
-    });
-
-    it('read failure', () => {
-        memory[AN_ADDRESS] = ANOTHER_VALUE;
-
-        assert.throw(() => {
-            driver.write(AN_ADDRESS, A_VALUE);
-        }, ReadFailureException);
-    });
-
-    it('checks if the hardware is ready after a write', () => {
-        let readSequenceForInitAddress = [BUSY, READY];
-        hardware.read = (address) => {
-            if (address === INIT_ADDRESS) {
-                return readSequenceForInitAddress.shift();
-            }
-
-            return A_VALUE;
-        }
-
-        driver.write(AN_ADDRESS, A_VALUE);
-
-        expect(readSequenceForInitAddress.length).to.equal(0);
-    });
-
-    it('timeout', () => {
-        hardware.read = () => BUSY
-
-        assert.throw(() => {
-            driver.write(AN_ADDRESS, ANOTHER_VALUE)
-        }, TimeoutException);
-    });
-
-    it('resets hardware when it is not ready after write', () => {
-        let readSequenceToTriggerReset = [TRIGGER_RESET, READY, ANOTHER_VALUE];
-        hardware.read = () => readSequenceToTriggerReset.shift();
-
-        driver.write(AN_ADDRESS, ANOTHER_VALUE);
-
-        const lastWrite = writeLog[writeLog.length - 1];
-        expect(lastWrite.address).to.equal(INIT_ADDRESS);
-        expect(lastWrite.data).to.equal(RESET_COMMAND);
-    });
-
-    it('triggers a VPP problem', () => {
-        memory[INIT_ADDRESS] = READY_BYTE_WITH_VPP_PROBLEM;
-
-        assert.throw(() => {
-            driver.write(AN_ADDRESS, A_VALUE);
-        }, VppException);
-    });
-
-    it('triggers an internal error', () => {
-        memory[INIT_ADDRESS] = READY_BYTE_WITH_INTERNAL_ERROR;
-
-        assert.throw(() => {
-            driver.write(AN_ADDRESS, A_VALUE);
-        }, InternalErrorException);
-    });
-
-    it('triggers a protected block error', () => {
-        memory[INIT_ADDRESS] = READY_BYTE_WITH_PROTECTED_BLOCK_PROBLEM;
-
-        assert.throw(() => {
-            driver.write(AN_ADDRESS, A_VALUE);
-        }, ProtectedBlockException);
     });
 
 });
